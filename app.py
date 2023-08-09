8ccc35b36192d6c3af8e0b1b115c8b5558fd01b0
Gizem Seval <gizemseval48@gmail.com>
2023-08-09 09:16:38 +0300

    main.py is renamed as app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+# from database.db_connection import connect_collection
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+
+
+app = Flask(__name__)
+
+# collection = connect_collection("ingredients")
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        if request.form.get("action1") == "breakfast":
+            return "OK"  # redirect(url_for("breakfast"))
+        else:
+            return "Not OK"  # redirect(url_for("dinner"))
+    return render_template("home.html")
+
+
+# @app.route("/dinner", methods=["GET", "POST"])
+# def dinner():
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
